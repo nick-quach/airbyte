2139b3a8ea4227d90a02c08d1f963031e3c1b963
nick-quach <nick.quach@veriday.com>
2022-12-12 18:02:03 -0500

    Added Incremental Streams where available

--- airbyte-integrations/connectors/source-harvest-forecast/source_harvest_forecast/streams.py
+++ airbyte-integrations/connectors/source-harvest-forecast/source_harvest_forecast/streams.py
@@ -56,8 +56,23 @@ class HarvestForecastStream(HttpStream, ABC):
         else:
             yield stream_data
 
+class IncrementalHarvestForecastStream(HarvestForecastStream, ABC):
 
-class Projects(HarvestForecastStream):
+    date_param_template = "%Y%m%d"
+    cursor_field = "updated_at"
+
+    def request_params(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, **kwargs) -> MutableMapping[str, Any]:
+        params = super().request_params(stream_state, **kwargs)
+        params = {**params, **stream_slice} if stream_slice else params
+        return params
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+        latest_benchmark = latest_record[self.cursor_field]
+        if current_stream_state.get(self.cursor_field):
+            return {self.cursor_field: max(latest_benchmark, current_stream_state[self.cursor_field])}
+        return {self.cursor_field: latest_benchmark}
+
+class Projects(IncrementalHarvestForecastStream):
     """
     Path: "https://api.forecastapp.com/projects"
     Docs: "This API is still in BETA and where is no official docs (https://help.getharvest.com/forecast/faqs/faq-list/api/)
@@ -78,14 +93,14 @@ class Roles(HarvestForecastStream):
     """
 
 
-class Milestones(HarvestForecastStream):
+class Milestones(IncrementalHarvestForecastStream):
     """
     Path: "https://api.forecastapp.com/milestones"
     Docs: "This API is still in BETA and where is no official docs (https://help.getharvest.com/forecast/faqs/faq-list/api/)
     """
 
 
-class People(HarvestForecastStream):
+class People(IncrementalHarvestForecastStream):
     """
     Path: "https://api.forecastapp.com/people"
     Docs: "This API is still in BETA and where is no official docs (https://help.getharvest.com/forecast/faqs/faq-list/api/)
@@ -94,17 +109,29 @@ class People(HarvestForecastStream):
 class HarvestForecastDateRangeStream(HarvestForecastStream):
 
     date_param_template = "%Y%m%d"
+    cursor_field = "updated_at"
 
     def request_params(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, **kwargs) -> MutableMapping[str, Any]:
         params = super().request_params(stream_state, **kwargs)
         params = {**params, **stream_slice} if stream_slice else params
         return params
 
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+        latest_benchmark = latest_record[self.cursor_field]
+        if current_stream_state.get(self.cursor_field):
+            return {self.cursor_field: max(latest_benchmark, current_stream_state[self.cursor_field])}
+        return {self.cursor_field: latest_benchmark}
+
     def stream_slices(self, sync_mode, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[MutableMapping[str, any]]]:
         """
         Override default stream_slices CDK method to provide date_slices as page chunks for data fetch.
         """
         slices = []
+
+        # Create first slice from 180days ago to today
+        start_date = pendulum.now().date().subtract(days=360)
+        end_date = pendulum.now().date().subtract(days=180)
+        slices.append({"start_date": start_date.strftime(self.date_param_template), "end_date": end_date.strftime(self.date_param_template)})
 
         # Create first slice from 180days ago to today
         start_date = pendulum.now().date().subtract(days=180)
